207ae73295798804046bce435a734cbdf893051e
FriendlyNPC <Devanyim@gmail.com>
2015-01-12 22:32:35 -0800

    Working example of material-style drawer

---
+++ app/src/main/java/com/school/comp3717/moviecollection/FragmentIndex.java
@@ -0,0 +1,30 @@
+package com.school.comp3717.moviecollection;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by neokree on 24/11/14.
+ *
+ * used for examples, will probably change or use something else later
+ * --Devan
+ */
+public class FragmentIndex extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView text = new TextView(this.getActivity());
+        text.setText("Section");
+        text.setGravity(Gravity.CENTER);
+        return text;
+
+    }
+
+}
--- app/src/main/java/com/school/comp3717/moviecollection/MainActivity.java
+++ app/src/main/java/com/school/comp3717/moviecollection/MainActivity.java
@@ -1,39 +1,55 @@
 package com.school.comp3717.moviecollection;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.MaterialSection;
+import it.neokree.materialnavigationdrawer.MaterialSectionListener;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
+public class MainActivity extends MaterialNavigationDrawer{
+    MaterialSection section1, section2, recorder, night, last;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    @SuppressWarnings("unchecked") //this.newSection causes warnings with MaterialNavigationDrawer types, but needed for example.
+    public void init(Bundle savedInstanceState) {
+        // create sections
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        section1 = this.newSection("Section 1",new FragmentIndex());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        section2 = this.newSection("Section 2",new MaterialSectionListener() {
+            @Override
+            public void onClick(MaterialSection section) {
+                Toast.makeText(MainActivity.this, "Section 2 Clicked", Toast.LENGTH_SHORT).show();
 
-        return super.onOptionsItemSelected(item);
+                // deselect section when is clicked
+                section.unSelect();
+            }
+        });
+        // recorder section with icon and 10 notifications
+        recorder = this.newSection("Recorder",this.getResources().getDrawable(R.drawable.ic_launcher),new FragmentIndex()).setNotifications(10);
+        // night section with icon, section color and notifications
+        night = this.newSection("Night Section", this.getResources().getDrawable(R.drawable.ic_launcher), new FragmentIndex())
+                .setSectionColor(Color.parseColor("#2196f3"),Color.parseColor("#1565c0")).setNotifications(150);
+        // night section with section color
+        last = this.newSection("Last Section", new FragmentButton()).setSectionColor(Color.parseColor("#ff9800"),Color.parseColor("#ef6c00"));
+
+        // add your sections to the drawer
+        this.addSection(section1);
+        this.addSection(section2);
+        this.addSubheader("Subheader");
+        this.addSection(recorder);
+        this.addSection(night);
+        this.addDivisor();
+        this.addSection(last);
+
+        /*
+        This keeps the drawer from being open when launched
+        material usually will launch the app with the drawer open the first couple
+        of times to let users know it uses the material-style drawer.
+         */
+        this.disableLearningPattern();
     }
 }
