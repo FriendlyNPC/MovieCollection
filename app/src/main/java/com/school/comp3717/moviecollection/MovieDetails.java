376e62297c230c8e245a93b4525f2b89abda4065
Derek <derekgleeson@hotmail.com>
2015-01-26 15:46:33 -0800

    Created Movie Details fragment and created a custom star rating

---
+++ app/src/main/java/com/school/comp3717/moviecollection/MovieDetails.java
@@ -0,0 +1,32 @@
+package com.school.comp3717.moviecollection;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieDetails extends Fragment {
+
+
+    public MovieDetails() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+    }
+
+
+
+
+}
