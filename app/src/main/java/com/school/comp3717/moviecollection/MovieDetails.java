ebc94b1b88b12f69e54074a84086b141c4c4475b
Derek <derekgleeson@hotmail.com>
2015-02-18 20:18:15 -0800

    Added tag lines to Movie Details and other small fixes

--- app/src/main/java/com/school/comp3717/moviecollection/MovieDetails.java
+++ app/src/main/java/com/school/comp3717/moviecollection/MovieDetails.java
@@ -16,17 +16,20 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.io.InputStream;
+import java.text.DecimalFormat;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MovieDetails extends Fragment {
 
+    private static final DecimalFormat RATING_FORMAT = new DecimalFormat("0.#");
     private static final int YEAR_LENGTH = 4;
 
     Movie movie;
     TextView title;
     TextView year;
+    TextView tagLine;
     TextView synopsis;
     TextView director;
     TextView userRating;
@@ -47,6 +50,7 @@ public class MovieDetails extends Fragment {
         poster = (ImageView) rootView.findViewById(R.id.moviePoster);
         title = (TextView) rootView.findViewById(R.id.movieTitle);
         year = (TextView) rootView.findViewById(R.id.movieYear);
+        tagLine = (TextView) rootView.findViewById(R.id.tagLine);
         synopsis = (TextView) rootView.findViewById(R.id.synopsisBody);
         director = (TextView) rootView.findViewById(R.id.movieDirector);
         userRating = (TextView) rootView.findViewById(R.id.userRatingText);
@@ -57,33 +61,73 @@ public class MovieDetails extends Fragment {
         movie = this.getArguments().getParcelable("movie");
         setDetails();
 
-        new DownloadPosterTask((ImageView) rootView.findViewById(R.id.moviePoster))
-                .execute("http://image.tmdb.org/t/p/w185" + movie.getPosterUrl());
+        if (movie.getPosterUrl() != null) {
+            new DownloadPosterTask((ImageView) rootView.findViewById(R.id.moviePoster))
+                    .execute("http://image.tmdb.org/t/p/w185" + movie.getPosterUrl());
+        }
 
         return rootView;
     }
 
     private void setDetails() {
         title.setText(movie.getTitle());
+        setYearText(movie, year);
+        setDirectorText(movie, director);
+        setTagLineText(movie, tagLine);
+        setSynopsisText(movie, synopsis);
+        synopsis.setText(movie.getSynopsis());
+        userRating.setText("User Rating:\n" + RATING_FORMAT.format(movie.getVoteAverage())
+                           + "/10 (" + movie.getVoteCount() + " votes)");
+        userRatingBar.setRating((float)movie.getVoteAverage() / 2);
+        myRatingBar.setRating((float)movie.getMyRating());
+        myReview.setText(movie.getMyReview());
+    }
 
-        year.setText("(" + movie.getReleaseDate().substring(0, YEAR_LENGTH) + ")");
-
+    private void setDirectorText(Movie movie, TextView director) {
         String directedBy = "Directed by";
         String[] directorArray = movie.getDirector().split("\t");
         int numDirectors = 0;
-        for(String directorName : directorArray){
+        for (String directorName : directorArray) {
             directedBy += (numDirectors == 0) ? " " : ", ";
             directedBy += directorName;
             numDirectors++;
         }
-        director.setText(directedBy);
+        directedBy = directedBy.trim();
+        if (directedBy.equals("Directed by")) {
+            director.setVisibility(View.GONE);
+        } else {
+            director.setVisibility(View.VISIBLE);
+            director.setText(directedBy);
+        }
+    }
 
-        synopsis.setText(movie.getSynopsis());
+    private void setTagLineText(Movie movie, TextView tagLine) {
+        String tagLineStr = movie.getTagLine();
+        if (tagLineStr == null || tagLineStr.isEmpty()) {
+            tagLine.setVisibility(View.GONE);
+        } else {
+            tagLine.setVisibility(View.VISIBLE);
+            tagLine.setText("\"" + tagLineStr + "\"");
+        }
+    }
 
-        userRating.setText(movie.getVoteAverage() + "/10 (" + movie.getVoteCount() + " votes)");
-        userRatingBar.setRating((float)movie.getVoteAverage() / 2);
-        myRatingBar.setRating((float)movie.getMyRating());
-        myReview.setText(movie.getMyReview());
+    private void setSynopsisText(Movie movie, TextView synopsis) {
+        String synopsisStr = movie.getSynopsis();
+        if (synopsisStr == null || synopsisStr.isEmpty()) {
+            synopsis.setVisibility(View.GONE);
+        } else {
+            synopsis.setVisibility(View.VISIBLE);
+            synopsis.setText(synopsisStr);
+        }
+    }
+
+    private void setYearText(Movie movie, TextView year) {
+        if (movie.getReleaseDate() == null || movie.getReleaseDate().isEmpty()) {
+            year.setVisibility(View.GONE);
+        } else {
+            year.setVisibility(View.VISIBLE);
+            year.setText("(" + movie.getReleaseDate().substring(0, YEAR_LENGTH) + ")");
+        }
     }
 
     private class DownloadPosterTask extends AsyncTask<String, Void, Bitmap> {
